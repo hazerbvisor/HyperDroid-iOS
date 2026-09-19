1231f3d68a484521ecaf640f4d6d2216161bdc15
hazerbvisor <hazerbvisor@gmail.com>
2026-09-19 11:35:21 +0800

    Add HyperDroid iOS app entry point

---
+++ Sources/HyperDroidIOS/HyperDroidIOSApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct HyperDroidIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            DesktopView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
