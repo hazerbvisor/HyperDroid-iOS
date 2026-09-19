45b4f85b098a37239607eeef81be304a5e3fde6a
hazerbvisor <hazerbvisor@gmail.com>
2026-09-19 12:21:56 +0800

    Embed XAPK UI atlas data

---
+++ Sources/HyperDroidIOS/HDAtlasData.swift
@@ -0,0 +1,14 @@
+enum HDAtlasData {
+    static let value =
+        HDAtlasChunk0.value +
+        HDAtlasChunk1.value +
+        HDAtlasChunk2.value +
+        HDAtlasChunk3.value +
+        HDAtlasChunk4.value +
+        HDAtlasChunk5.value +
+        HDAtlasChunk6.value +
+        HDAtlasChunk7.value +
+        HDAtlasChunk8.value +
+        HDAtlasChunk9.value +
+        HDAtlasChunk10.value
+}
